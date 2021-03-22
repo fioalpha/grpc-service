a7fba52320ad4521270d619d280a71548d9c9e08
fioalpha <fioalpha@gmail.com>
2021-03-22 08:01:23 -0300

    djsakg

--- client-gprc.js
+++ client-gprc.js
@@ -3,8 +3,8 @@ const grpc = require('grpc')
 const PROTO_PATH = './task.proto'
 const NoteService = grpc.load(PROTO_PATH).TaskService
 
-// const IP = '165.232.147.56'
-const IP = '127.0.0.1'
+const IP = '165.232.147.56'
+// const IP = '127.0.0.1'
 
 const client = new NoteService(`${IP}:50051`, grpc.credentials.createInsecure())
     
